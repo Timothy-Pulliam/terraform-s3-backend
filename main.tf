52e2d6f8f29389609f66b69e55b47724e2e42a27
Timothy Pulliam <timothy.dpulliam@gmail.com>
2024-03-03 18:41:41 -0500

    specify hashicorp/random provider version

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
 
   #   Uncomment after creating the backend, then run `terraform init`
